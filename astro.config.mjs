eafc5778ac624b2193366ed4a468bca44826fb32
Adrian Prado <adrian.prado@gmail.com>
2025-09-25 22:38:02 -0400

    fix: nuevo pipeline para nueva pagina

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://dagklassical.gitlab.io',
-  base: '/dag-www'
+  base: '/dag-www',
+  outDir: 'public'  // ← FORZAR que genere en public/
 });
