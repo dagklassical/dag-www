546fb47665b904344240832b5c18dc5b2f92d8c1
Adrián Prado - El Sistema <aprado@elsistema.org.ve>
2025-10-05 23:46:11 -0400

    docs: cambios del remote de usuario a organización dagklassical

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // Configuración para GitLab Pages
-  site: 'https://aprado4.gitlab.io',
+  site: 'https://dagklassical.gitlab.io',
   base: '/dag-www',
   
   // Configuración de build
