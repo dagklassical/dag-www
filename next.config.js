1ff38e9d65f47b7d8bf46731303112ac9c6e7f21
apradoc-es <apradoc-es@users.noreply.github.com>
2025-11-15 17:27:27 -0400

    Rescate Revert "🔧 Fix: Eliminar basePath para servir desde raíz en GitHub Pages"
    This reverts commit 52a5db5dad0faec487600809086b6afd5b4789df.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 // next.config.js
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: { 
-    unoptimized: true 
-  },
-  // BasePath y assetPrefix eliminados para servir desde la raíz
+output: 'export',
+trailingSlash: true,
+images: { 
+unoptimized: true 
+},
+basePath: process.env.NODE_ENV === 'production' ? '/dag-www' : '',
+assetPrefix: process.env.NODE_ENV === 'production' ? '/dag-www' : '',
 }
 module.exports = nextConfig
